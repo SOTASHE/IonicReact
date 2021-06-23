61e7d22997ded4e1de37ddf5778bfdedcff999e5
Sinaye Sotashe <sinayesotatshe@gmail.com>
2021-06-24 01:27:18 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mock_project_setup',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
